e40f36276bd7bc20b860e1227fe780bf38231f3d
dropfred <drop.fred.10@gmail.com>
2021-07-28 10:55:21 +0200

    Update sha3.cpp

--- sha3.cpp
+++ sha3.cpp
@@ -4,35 +4,30 @@
 #include <cstring>
 #include <cstdint>
 
-#define SHA3_TRANSPOSE_NONE 0
-#define SHA3_TRANSPOSE_STATE 1
-#define SHA3_TRANSPOSE_INPUT 2
-#define SHA3_TRANSPOSE SHA3_TRANSPOSE_NONE
-
 namespace
 {
     std::uint64_t const keccak_rc[24] =
     {
         0x0000000000000001ULL,
         0x0000000000008082ULL,
-        0x800000000000808AULL,
+        0x800000000000808aULL,
         0x8000000080008000ULL,
-        0x000000000000808BULL,
+        0x000000000000808bULL,
         0x0000000080000001ULL,
         0x8000000080008081ULL,
         0x8000000000008009ULL,
-        0x000000000000008AULL,
+        0x000000000000008aULL,
         0x0000000000000088ULL,
         0x0000000080008009ULL,
-        0x000000008000000AULL,
-        0x000000008000808BULL,
-        0x800000000000008BULL,
+        0x000000008000000aULL,
+        0x000000008000808bULL,
+        0x800000000000008bULL,
         0x8000000000008089ULL,
         0x8000000000008003ULL,
         0x8000000000008002ULL,
         0x8000000000000080ULL,
-        0x000000000000800AULL,
-        0x800000008000000AULL,
+        0x000000000000800aULL,
+        0x800000008000000aULL,
         0x8000000080008081ULL,
         0x8000000000008080ULL,
         0x0000000080000001ULL,
@@ -48,185 +43,102 @@ namespace
         {27, 20, 39, 8, 14}
     };
 
-#if (SHA3_TRANSPOSE == SHA3_TRANSPOSE_STATE)
-    void transpose(std::uint64_t const s[5][5], std::uint64_t d[5][5])
-    {
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            for (std::size_t y = 0; y < 5; ++y)
-            {
-                d[x][y] = s[y][x];
-            }
-        }
-    }
-#elif (SHA3_TRANSPOSE == SHA3_TRANSPOSE_INPUT)
-    std::size_t transpose(std::size_t i)
-    {
-        // it appears that gcc doesn't optimize div(x, 8), manually use and/shift instead
-        auto qr = std::div(int(i >> 3), 5);
-        return (((qr.rem * 5) + qr.quot) * 8 + (i & 7));
-    }
-#endif
-
-    std::uint64_t keccak_rotl(std::uint64_t x, std::uint64_t y)
+    std::uint64_t rotl(std::uint64_t x, std::uint64_t y)
     {
         return ((x << y) | (x >> (64 - y)));
     }
 
-#if (SHA3_TRANSPOSE == SHA3_TRANSPOSE_NONE)
-    void keccak_round(std::uint64_t state[5][5], std::uint64_t rc)
+    class sha3
     {
-        std::uint64_t bcd[5][5];
-        /* theta */
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            bcd[0][x] = state[0][x] ^ state[1][x] ^ state[2][x] ^ state[3][x] ^ state[4][x];
-        }
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            std::uint64_t v = bcd[0][(x + 4) % 5] ^ keccak_rotl(bcd[0][(x + 1) % 5], 1);
-            for (std::size_t y = 0; y < 5; ++y)
-            {
-                state[y][x] ^= v;
-            }
-        }
-        /* rho pi */
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            for (std::size_t y = 0; y < 5; ++y)
-            {
-                bcd[((2 * x) + (3 * y)) % 5][y] = keccak_rotl(state[y][x], keccak_rot[x][y]);
-            }
-        }
-        /* chi */
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            for (std::size_t y = 0; y < 5; ++y)
-            {
-                state[y][x] = bcd[y][x] ^ ((~bcd[y][(x + 1) % 5] & bcd[y][(x + 2) % 5]));
-            }
-        }
-        /* iota */
-        state[0][0] ^= rc;
-    }
-#else
-    void keccak_round(std::uint64_t state[5][5], std::uint64_t rc)
-    {
-        std::uint64_t bcd[5][5];
-        /* theta */
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            bcd[x][0] = state[x][0] ^ state[x][1] ^ state[x][2] ^ state[x][3] ^ state[x][4];
-        }
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            std::uint64_t v = bcd[(x + 4) % 5][0] ^ keccak_rotl(bcd[(x + 1) % 5][0], 1);
-            for (std::size_t y = 0; y < 5; ++y)
-            {
-                state[x][y] ^= v;
-            }
-        }
-        /* rho pi */
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            for (std::size_t y = 0; y < 5; ++y)
-            {
-                bcd[y][((2 * x) + (3 * y)) % 5] = keccak_rotl(state[x][y], keccak_rot[x][y]);
-            }
-        }
-        /* chi */
-        for (std::size_t x = 0; x < 5; ++x)
-        {
-            for (std::size_t y = 0; y < 5; ++y)
-            {
-                state[x][y] = bcd[x][y] ^ ((~bcd[(x + 1) % 5][y] & bcd[(x + 2) % 5][y]));
-            }
-        }
-        /* iota */
-        state[0][0] ^= rc;
-    }
-#endif
-
-    void keccak_f(std::uint64_t state[5][5])
-    {
-#if (SHA3_TRANSPOSE == SHA3_TRANSPOSE_STATE)
-        std::uint64_t ST[5][5];
-        auto s = ST;
-        transpose(state, s);
-#else
-        auto s = state;
-#endif
-        for (std::size_t r = 0; r < 24; ++r)
-        {
-            keccak_round(s, keccak_rc[r]);
-        }
-#if (SHA3_TRANSPOSE == SHA3_TRANSPOSE_STATE)
-        transpose(s, state);
-#endif
-    }
-
-    struct sha3
-    {
-        std::uint8_t digest;
-        std::uint8_t block;
+        std::size_t digest;
+        std::size_t block;
         std::uint64_t index;
         std::uint64_t state[5][5];
-    };
 
-    void init(sha3 &sha, std::uint8_t digest_size)
-    {
-        std::memset(sha.state, 0, 5 * 5 * 8);
-        sha.index = 0;
-        sha.digest = digest_size;
-        sha.block = (5 * 5 * 8) - (2 * digest_size);
-    }
-
-    void update(sha3 &sha, const std::uint8_t *input, std::size_t size)
-    {
-        auto * state = reinterpret_cast<std::uint8_t *>(sha.state);
-
-        for (std::uint32_t i = 0; i < size; i++)
+        void keccak_round(std::uint64_t r)
         {
-#if (SHA3_TRANSPOSE == SHA3_TRANSPOSE_INPUT)
-            state[transpose(sha.index++)] ^= input[i];
-#else
-            state[sha.index++] ^= input[i];
-#endif
-            if (sha.index == sha.block)
+            std::uint64_t bcd[5][5];
+            /* theta */
+            for (std::size_t x = 0; x < 5; ++x)
             {
-                keccak_f(sha.state);
-                sha.index = 0;
+                bcd[0][x] = state[0][x] ^ state[1][x] ^ state[2][x] ^ state[3][x] ^ state[4][x];
+            }
+            for (std::size_t x = 0; x < 5; ++x)
+            {
+                std::uint64_t v = bcd[0][(x + 4) % 5] ^ rotl(bcd[0][(x + 1) % 5], 1);
+                for (std::size_t y = 0; y < 5; ++y)
+                {
+                    state[y][x] ^= v;
+                }
+            }
+            /* rho pi */
+            for (std::size_t x = 0; x < 5; ++x)
+            {
+                for (std::size_t y = 0; y < 5; ++y)
+                {
+                    bcd[((2 * x) + (3 * y)) % 5][y] = rotl(state[y][x], keccak_rot[x][y]);
+                }
+            }
+            /* chi */
+            for (std::size_t x = 0; x < 5; ++x)
+            {
+                for (std::size_t y = 0; y < 5; ++y)
+                {
+                    state[y][x] = bcd[y][x] ^ ((~bcd[y][(x + 1) % 5] & bcd[y][(x + 2) % 5]));
+                }
+            }
+            /* iota */
+            state[0][0] ^= r;
+        }
+
+        void keccak_f()
+        {
+            for (std::size_t r = 0; r < 24; ++r)
+            {
+                keccak_round(keccak_rc[r]);
             }
         }
-    }
 
-    void finalize(sha3 &sha, std::uint8_t *output)
-    {
-        std::uint8_t * state = reinterpret_cast<std::uint8_t *>(sha.state);
-#if (SHA3_TRANSPOSE == SHA3_TRANSPOSE_INPUT)
-        state[transpose(sha.index)] ^= 0x06;
-        state[transpose(sha.block - 1)] ^= 0x80;
-#else
-        state[sha.index] ^= 0x06;
-        state[sha.block - 1] ^= 0x80;
-#endif
-        keccak_f(sha.state);
-#if (SHA3_TRANSPOSE == SHA3_TRANSPOSE_INPUT)
-        for (std::size_t i = 0; i < 4; ++i)
+    public:
+        void begin(std::size_t size)
         {
-            std::memcpy(output + (i * 8), sha.state[i], 8);
+            std::memset(state, 0, 5 * 5 * 8);
+            index = 0;
+            digest = size;
+            block = (5 * 5 * 8) - (2 * size);
         }
-#else
-        std::memcpy(output, state, sha.digest);
-#endif
-    }
+
+        void append(std::uint8_t const *input, std::size_t size)
+        {
+            auto *s = reinterpret_cast<std::uint8_t *>(state);
+
+            for (std::uint32_t i = 0; i < size; i++)
+            {
+                s[index++] ^= input[i];
+                if (index == block)
+                {
+                    keccak_f();
+                    index = 0;
+                }
+            }
+        }
+
+        void end(std::uint8_t *output)
+        {
+            std::uint8_t *s = reinterpret_cast<std::uint8_t *>(state);
+            s[index] ^= 0x06;
+            s[block - 1] ^= 0x80;
+            keccak_f();
+            std::memcpy(output, state, digest);
+        }
+    };
 
     void sha3_x(void const *data, std::size_t data_size, void *digest, std::size_t digest_size)
     {
         sha3 sha;
-        init(sha, digest_size);
-        update(sha, reinterpret_cast<std::uint8_t const *>(data), data_size);
-        finalize(sha, reinterpret_cast<std::uint8_t *>(digest));
+        sha.begin(digest_size);
+        sha.append(reinterpret_cast<std::uint8_t const *>(data), data_size);
+        sha.end(reinterpret_cast<std::uint8_t *>(digest));
     }
 }
 
